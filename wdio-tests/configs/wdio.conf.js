6f2030f261883fb65f8e77bdd06ee0daa2ed9de9
zilurrane <zilurrane@gmail.com>
2018-06-03 00:34:28 +0530

    Clean-up

--- wdio-tests/configs/wdio.conf.js
+++ wdio-tests/configs/wdio.conf.js
@@ -38,14 +38,15 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
-  capabilities: [{
-    maxInstances: 5,
-    browserName: 'firefox'
-  },
-  {
-    maxInstances: 5,
-    browserName: 'chrome'
-  }
+  capabilities: [
+    //   {
+    //   maxInstances: 5,
+    //   browserName: 'firefox'
+    // },
+    {
+      maxInstances: 5,
+      browserName: 'chrome'
+    }
   ],
   //
   // ===================
